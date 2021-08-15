5b0ae8a7862f06d0c211cf99d41cad2299d0fd0c
Thuandeptrai <tvc4567@gmail.com>
2021-08-15 19:57:33 +0700

    fixed-something

--- src/HomePage.js
+++ src/HomePage.js
@@ -13,9 +13,10 @@ export default function HomePage() {
     return (
         <div className="h-screen bg-gray-100 h-screen flex justify-center">
     <div className="py-6 px-8 h-50 mt-20 bg-white rounded shadow-xl">
+        <h1  className="font-bold text-center mb-2 text-xl" > Auto-Saving Text </h1>
       <form action="">
-        <div className="mb-6">
-          <label htmlFor="name" className="block text-gray-800 w-500 font-bold">Name:</label>
+        <div className="mb-6  w-80">
+          <label htmlFor="name" className="block text-gray-800  font-bold">Name:</label>
           <input type="text" name="name" id="name"  onChange={(e)=> {setHeader(e.target.value)}} placeholder="" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" />
         </div>
 
